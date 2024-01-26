8f666eb6dcf00ecb555f7dfe52cedc5e65a6d9a6
mb20ot <133376358+mb20ot@users.noreply.github.com>
2024-01-25 20:34:02 -0500

    fix typos

--- backend/src/backend/routes.py
+++ backend/src/backend/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from auth import api_key_auth
-from summerize import summerize_from_article, summerize_from_video
-from models import Login, Register, Summerize
+from summarize import summarize_from_article, summarize_from_video
+from models import Login, Register, Summarize
 from fastapi.middleware.cors import CORSMiddleware
 from auth import google, google_callback, login, register
 
@@ -14,8 +14,8 @@ class Routes:
         self.router.add_api_route("/google/callback", self.google_callback_route, methods=["GET"])
         self.router.add_api_route("/register", self.register_route, methods=["POST"])
         self.router.add_api_route("/login", self.login_route, methods=["POST"])
-        self.router.add_api_route("/summerize/article", self.summerize_article_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
-        self.router.add_api_route("/summerize/video", self.summerize_video_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
+        self.router.add_api_route("/summarize/article", self.summarize_article_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
+        self.router.add_api_route("/summarize/video", self.summarize_video_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
         self.router.add_api_route("/shorten", self.shorten_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
         pass
     def get_router(self):
@@ -24,21 +24,19 @@ class Routes:
         return google()
     def google_callback_route(self):
         return google_callback()
-    
+
     def register_route(self, form_data: Register):
         return register(form_data.email, form_data.password, form_data.full_name)
     def login_route(self, form_data: Login):
         print(form_data)
         return login(form_data.email, form_data.password)
-    
 
-    def shorten_route(self, form_data: Summerize):
+
+    def shorten_route(self, form_data: Summarize):
         return f'<h1>Shorten Page</h1>'
 
 
-    def summerize_article_route(self, form_data: Summerize):
-        return summerize_from_article(form_data.url)
-    def summerize_video_route(self, form_data: Summerize):
-        return summerize_from_video(form_data.url)
-
-
+    def summarize_article_route(self, form_data: Summarize):
+        return summarize_from_article(form_data.url)
+    def summarize_video_route(self, form_data: Summarize):
+        return summarize_from_video(form_data.url)
--- backend/src/backend/summerize.py
+++
@@ -1,7 +0,0 @@
-
-def summerize_from_article(url: str) -> str:
-    return f"<h1>Summerize Page {url} </h1>"
-
-
-def summerize_from_video(url: str) -> str:
-    return f"<h1>Summerize Page {url} </h1>"

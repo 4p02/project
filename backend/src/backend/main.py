f8ad21cebb89a6330fdd8422da7cd8a3fbfc7fd9
mb20ot <133376358+mb20ot@users.noreply.github.com>
2024-02-04 18:21:19 -0500

    make main async and connect to db there

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,53 +1,58 @@
 import fastapi
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 import uvicorn
 
+from backend import config, logger
+from backend.db import Database
+from backend.misc import asyncio_entrypoint
 from auth import google, google_callback, register, login
 from models import Login, Register, Summarize
 from routes import Routes
-from backend import config, logger
-from starlette.middleware.sessions import SessionMiddleware
 
 
-def main():
-    try:
+@asyncio_entrypoint
+async def main():
+    async with (await Database.connect()) as db:
+        logger.debug("db connected")
+
         logger.debug("bringing up fastapi")
-
         app = fastapi.FastAPI()
         router: fastapi.APIRouter = Routes().get_router()
         app.include_router(router)
         app.add_middleware(SessionMiddleware, secret_key=config.jwt_secret)
-        uvicorn.run(
-            app=app,
-            host=config.api.host,
-            port=config.api.port,
-            use_colors=False, access_log=True,
-            log_config={
-                "version": 1,
-                "disable_existing_loggers": False,
-                "formatters": {
-                    "default": {
-                        "()": "uvicorn.logging.DefaultFormatter",
-                        "fmt": "%(levelprefix)s %(message)s",
-                        "use_colors": None,
-                    },
-                    "access": {
-                        "()": "uvicorn.logging.AccessFormatter",
-                        "fmt": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',
-                    },
-                },
-                "handlers": {},
-                "loggers": {
-                    "uvicorn": {"level": "DEBUG", "propagate": True},
-                    "uvicorn.error": {"level": "DEBUG", "propagate": True},
-                    "uvicorn.access": {"level": "DEBUG", "propagate": True},
-                },
-            }
-        )
-    except Exception as e:
-        
-        print(e)
 
+        server = uvicorn.Server(
+            uvicorn.Config(
+                app=app,
+                host=config.api.host,
+                port=config.api.port,
+                use_colors=False,
+                access_log=True,
+                log_config={
+                    "version": 1,
+                    "disable_existing_loggers": False,
+                    "formatters": {
+                        "default": {
+                            "()": "uvicorn.logging.DefaultFormatter",
+                            "fmt": "%(levelprefix)s %(message)s",
+                            "use_colors": None,
+                        },
+                        "access": {
+                            "()": "uvicorn.logging.AccessFormatter",
+                            "fmt": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',
+                        },
+                    },
+                    "handlers": {},
+                    "loggers": {
+                        "uvicorn": {"level": "DEBUG", "propagate": True},
+                        "uvicorn.error": {"level": "DEBUG", "propagate": True},
+                        "uvicorn.access": {"level": "DEBUG", "propagate": True},
+                    },
+                }
+            )
+        )
+        await server.serve()
 
 if __name__ == "__main__":
     main()

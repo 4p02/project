1a18afa8db80fec775282b0cb030d679696459fb
mb20ot <133376358+mb20ot@users.noreply.github.com>
2024-03-02 16:50:19 -0500

    create auth middleware to auth using tokens

--- backend/src/backend/routes.py
+++ backend/src/backend/routes.py
@@ -1,12 +1,48 @@
+from typing import Optional, Tuple
+
 from fastapi import APIRouter, Depends, Request, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.sessions import SessionMiddleware
+
+from starlette.authentication import (
+    AuthCredentials, AuthenticationBackend, AuthenticationError, BaseUser,
+    requires
+)
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.requests import HTTPConnection
+
+from jwt.exceptions import JWTDecodeError
 
 from backend import config
-from backend.auth import api_key_auth, google, google_callback, login, register
+from backend.auth import api_key_auth, google, google_callback, login, register, Token
 from backend.db import Database
 from backend.models import Login, Register, Summarize
 from backend.misc import handle_and_log_exceptions
+
+
+class JWTUser(BaseUser):
+    token: Token
+    def __init__(self, token: Token) -> None: self.token = token
+
+    @property
+    def is_authenticated(self) -> bool: return True
+
+
+class JWTAuthBackend(AuthenticationBackend):
+    async def authenticate(self, conn: HTTPConnection) -> Optional[Tuple[AuthCredentials, BaseUser]]:
+        if "Authorization" not in conn.headers: return None
+
+        parts = conn.headers["Authorization"].split(" ")
+        if len(parts) != 2: raise AuthenticationError("Invalid authorization header")
+        scheme, token = parts
+
+        if scheme.lower() != 'bearer': raise AuthenticationError("Invalid authorization header")
+
+        try:
+            Token.decode(token)
+        except JWTDecodeError as ex:
+            raise AuthenticationError("Invalid or expired token")
+
+        return (AuthCredentials(["authenticated"]), JWTUser(token))
 
 
 class Routes:
@@ -33,24 +69,25 @@ class Routes:
 
         self.router.add_api_route("/google", self.google_route, methods=["GET"])
         self.router.add_api_route("/google/callback", self.google_callback_route, methods=["GET"])
-        self.router.add_api_route("/register", self.register_route, methods=["POST"])
-        self.router.add_api_route("/login", self.login_route, methods=["POST"])
-        self.router.add_api_route("/get_new_token", self.get_new_token_route, methods=["POST"])
+        self.router.add_api_route("/auth/register", self.register_route, methods=["POST"])
+        self.router.add_api_route("/auth/login", self.login_route, methods=["POST"])
+        self.router.add_api_route("/auth/refresh", self.refresh_route, methods=["POST"])
 
         self.router.add_api_route("/summarize/article", self.summarize_article_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
         self.router.add_api_route("/summarize/video", self.summarize_video_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
         self.router.add_api_route("/shorten", self.shorten_route, methods=["POST"], dependencies=[Depends(api_key_auth)])
-        self.app.include_router(self.router)
-        self.app.add_middleware(SessionMiddleware, secret_key=config.auth.jwt_secret)
 
-    
+        self.app.include_router(self.router)
+        self.app.add_middleware(AuthenticationMiddleware, backend=JWTAuthBackend())
+
+
     @handle_and_log_exceptions(reraise=HTTPException(500, "Internal server error :("))
     def google_route(self, request: Request):
         """
         Redirects to Google OAuth.
         """
         return google(request=request)
-    
+
     @handle_and_log_exceptions(reraise=HTTPException(500, "Internal server error :("))
     def google_callback_route(self, request: Request):
         """
@@ -73,14 +110,14 @@ class Routes:
         print(form_data)
         return login(form_data.email, form_data.password)
 
+    @requires(["authenticated"])
     @handle_and_log_exceptions(reraise=HTTPException(500, "Internal server error :("))
-    def get_new_token_route(self):
+    def refresh_route(self, request: Request):
         """
         Generates a new JWT token for the user.
         """
         return f'<h1>Get New Token Page</h1>'
 
-    
 
     @handle_and_log_exceptions(reraise=HTTPException(500, "Internal server error :("))
     def shorten_route(self, form_data: Summarize):

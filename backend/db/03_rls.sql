7189763bec3775956fa536bcdbb8c3c5c461e890
mb20ot <133376358+mb20ot@users.noreply.github.com>
2024-03-02 22:02:27 -0500

    implement rls for users and articles

---
+++ backend/db/03_rls.sql
@@ -0,0 +1,32 @@
+alter table private.users enable row level security;
+alter table public.documents enable row level security;
+alter table public.articles enable row level security;
+alter table public.links enable row level security;
+
+
+-- no reason to grant anon access; they can't query or update anything
+grant select, update(email, fullname) on public.users to pgrest_auth;
+grant select(id, created_at, email, fullname), update(email, fullname) on private.users to pgrest_auth;
+
+-- since the view was created with security_invoker, policies from the
+-- impersonated role will be applied (instead of view author's permissions).
+-- so, this policy will apply to both private.users and public.users
+create policy public_users_rls on private.users as restrictive for select
+    using (id = (current_setting('request.jwt.claims', true)::json->>'uid')::int);
+
+
+grant select on public.documents to pgrest_auth;
+grant select on public.documents to pgrest_anon;
+-- create policy public_documents_rls on public.documents as permissive for select
+--     using (
+--         select * from public.articles where owner =
+--         owner = current_setting('request.jwt.claims', true)::json->>'uid'
+--     );
+
+
+grant select, update(private), delete on public.articles to pgrest_auth;
+grant select, update(private), delete on public.articles to pgrest_anon;
+
+create policy public_articles_rls on public.articles as permissive for all
+    using ((not private) or (owner = (current_setting('request.jwt.claims', true)::json->>'uid')::int))
+    with check (owner = (current_setting('request.jwt.claims', true)::json->>'uid')::int);

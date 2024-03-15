dab9a00b3c2773a64523ede4e4ae0768a4b15983
FieryAced <35231626+FieryAced@users.noreply.github.com>
2024-03-14 22:04:11 -0400

    Update summarize.py

--- backend/src/backend/summarize.py
+++ backend/src/backend/summarize.py
@@ -15,8 +15,14 @@ class Summerize:
 def summary_prompt(text: str) -> str:
     return f"Take a deep breath and summarize the following text:\n{text}"
 
-def vidoe_recursive_summary_prompt(text: str, summary_history: str = "") -> str:
-    
+
+
+def video_recursive_summary_prompt(text: str, summary_history: str = "") -> str:
+    """
+    Generates the prompt for recursive prompt-based moving/expanding context window summarization for videos
+
+    Please use only when context window is too small for transcript size.
+    """
     prompt = f"Below is a part of the transcript of the video that has yet to be summarized:\n{text}\n\nSummarize this transcript"
     if len(summary_history) != 0:
         prompt = f"Below is the summary of the video so far, please keep this summary in mind when summarizing following parts of the video:\n{summary_history}\n\n" + prompt + " while keeping the summary of the video so far in mind"
